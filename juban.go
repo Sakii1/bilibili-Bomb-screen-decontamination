5bca9ef9c420d23a7a4f5c6958df34923b22a418
Sakii1 <107802714+Sakii1@users.noreply.github.com>
2023-01-09 05:06:00 +0800

    Update juban.go

--- juban.go
+++ juban.go
@@ -13,8 +13,8 @@ import (
 
 var room int64 = 21195828 //输入房间号  这也找不到建议速速remake
 
-//cookie1=  后面请直接放内容  不要放入cookie:  直接从 buvid3= 或 _uuid=开始复制完成就行
-//csrf1=  后面请直接放内容  在cookie的bili_jct里面  不要放入 bili_jct=
+//cookie1=  后面请直接放内容  不要放入cookie:  直接从 buvid3= 或 _uuid=开始复制完全就行
+//csrf1=  后面请直接放内容  在cookie数据里面的bili_jct后面  不要放入 bili_jct=
 //如果一个 举办成功 都没看到  那应该是数据放错了
 
 var cookie1 string = "" //
